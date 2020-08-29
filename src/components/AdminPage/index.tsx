b16d73a312bda73a343f4cae18ce18d9fc17c47c
Pedro <www.psro@gmail.com>
2020-08-29 11:03:47 -0300

    AdminPage refactored using EmployeePage

--- src/components/AdminPage/index.tsx
+++ src/components/AdminPage/index.tsx
@@ -1,55 +1,25 @@
 import React from 'react';
-import Navbar from '../Navbar';
-import Breadcrumb from '../Breadcrumb';
-import { Link } from 'react-router-dom';
+import EmployeePage from '../EmployeePage';
 
 interface AdminPageProps {
     adminSection?: string;
 }
 
 const AdminPage: React.FC<AdminPageProps> = props => {
-    const sections = ["Geral", "Pessoas", "Aulas", "Acesso"];
-    const activeSection = props.adminSection?? sections[0];
-
-    const fullscreen = {
-        height: '100vh',
-    };
-
-    const whiteBackground = {
-        backgroundColor: 'white',
-    };
-
+    const adminSections = [
+        "Home",
+        "Pessoas",
+        "Turmas",
+        "Acesso"
+    ];
     return (
-        <section style={fullscreen} className="bg-gray">
-            <div style={whiteBackground}>
-                <Navbar navbarLight={true}>
-                    <Breadcrumb address={`admin/${props.adminSection?? ''}`} />
-                </Navbar>
-
-                <ul className="tab tab-block">
-                    {
-                        sections.map(section => {
-                            return (
-                                <li key={`li-admin-${section}`} className="tab-item">
-                                    <Link 
-                                        to={section !== sections[0]? `/admin/${section.toLowerCase()}` : '/admin'}
-                                        className={section === activeSection? 'active' : ''}
-                                    >
-                                        {section}
-                                    </Link>
-                                </li>
-                            );
-                        })
-                    }
-                </ul>
-            </div>
-
-            <div className="container bg-gray">
-                {props.children}
-            </div>
-            
-        </section>
-
+        <EmployeePage 
+            employeeType="Admin"
+            sections={adminSections}
+            currentSection={props.adminSection}
+        >
+            {props.children}
+        </EmployeePage>
     );
 }
 
