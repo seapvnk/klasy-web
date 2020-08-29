4a1caa7ca1166ada847fcb1a675386d85fe43331
Pedro <www.psro@gmail.com>
2020-08-29 16:24:12 -0300

    AdminManagePeople section updated

--- src/pages/admin/AdminManagePeople/index.tsx
+++ src/pages/admin/AdminManagePeople/index.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import AdminPage from '../../../components/AdminPage';
+import ProfileCard from '../../../components/ProfileCard';
 
 function AdminManagePeople() {
     return(
         <AdminPage adminSection="Pessoas">
-            Hey Folks!
+            <ProfileCard
+                username="Pedro"
+                gender="M"
+                bio="Programando, bebendo café e sendo muito chato o dia todinho..."
+                type="Professor"
+            />            
+            <ProfileCard
+                username="Rei da didática"
+                bio="PIPI POPOPO PIPI POPOPO, Eu falo e ninguem escuta!"
+                type="Professor"
+            />            
+            <ProfileCard
+                username="NeusaGOD666"
+                gender="F"
+                bio="Como muda o nome? Meu neto colocou esse nome do demônio???"
+                type="Aluno"
+            />            
         </AdminPage>
     );
 }
