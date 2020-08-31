b9bb01a8e06e5951ace0c54ed7ac2eda1b959ce9
Pedro <www.psro@gmail.com>
2020-08-31 15:53:25 -0300

    New route created: /turmas

--- src/pages/AdminRoutes/index.tsx
+++ src/pages/AdminRoutes/index.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Route,  } from 'react-router-dom';
 
 import AdminHome from '../admin/AdminHome';
 import AdminManagePeople from '../admin/AdminManagePeople';
+import AdminManageClasses from '../admin/AdminManageClasses';
 
 function AdminRoutes() {
     return (
         <BrowserRouter>
             <Route exact path='/admin' component={AdminHome}/>
             <Route exact path='/admin/pessoas' component={AdminManagePeople}/>
+            <Route exact path='/admin/turmas' component={AdminManageClasses}/>
         </BrowserRouter>
     );
 }
