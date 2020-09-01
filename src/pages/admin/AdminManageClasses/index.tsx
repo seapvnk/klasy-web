06951733527cfe85e71de79b3cda4197f03969de
Pedro <www.psro@gmail.com>
2020-08-31 22:48:21 -0300

    AdminManageClasses using Form component

--- src/pages/admin/AdminManageClasses/index.tsx
+++ src/pages/admin/AdminManageClasses/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import AdminPage from '../../../components/AdminPage';
+import Form from '../../../components/Form';
 
 function AdminManageClasses() {
     return(
         <AdminPage adminSection="Turmas">
-            There's no place like home.
+            <Form
+                buttonMessage="Criar nova turma"
+                fields={[
+                    {label: "Nome da turma", name: "name"},
+                ]} 
+            />
         </AdminPage>
     );
 }
