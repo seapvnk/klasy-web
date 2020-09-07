4de3334deac7b2ace5ae74e88c2e527b2d61c630
Pedro <www.psro@gmail.com>
2020-09-07 15:54:56 -0300

    ProfileList updating when remove profile

--- src/components/ProfileList/index.tsx
+++ src/components/ProfileList/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ProfileCard, { ProfileCardProps } from '../ProfileCard';
 import PaginationList from '../PaginationList';
 
@@ -8,17 +8,30 @@ interface ProfileListProps {
 }
 
 const ProfileList: React.FC<ProfileListProps> = ({ profiles, numberOfPages }) => {
+    const [profilesData, setProfileData] = useState(profiles);
+    const [removedProfile, setRemovedProfile] = useState(-1);
+    const onRemove = (id: number) => {
+        setRemovedProfile(id);
+    }
+    
+    
+    useEffect(() => {
+        setProfileData(profilesData.filter((profile) => profile.id !== removedProfile))
+    }, [ removedProfile ] );
+
     return (
-        <PaginationList link="Admin/pessoas" numberOfPages={numberOfPages}>
+        <PaginationList link="Admin/Pessoas" numberOfPages={numberOfPages}>
             {
-                profiles.map( (profile, index) => {
+                profilesData.map( ({id, username, bio, type, gender}, index) => {
                     return (
                         <ProfileCard
+                            id={id}
                             key={index}
-                            username={profile.username}
-                            bio={profile.bio}
-                            type={profile.type}
-                            gender={profile.gender}
+                            username={username}
+                            bio={bio}
+                            type={type}
+                            gender={gender}
+                            onRemove={() => onRemove(id)}
                         />
                     )
                 })
