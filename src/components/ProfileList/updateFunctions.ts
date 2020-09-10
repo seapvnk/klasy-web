9a846a76d0c11d8f85ff56fc78ef61c8804fc8e8
Pedro <www.psro@gmail.com>
2020-09-10 12:21:29 -0300

    ProfileList: Update functions in another file

---
+++ src/components/ProfileList/updateFunctions.ts
@@ -0,0 +1,18 @@
+import UpdateOperation from '../../utils/UpdateOperation';
+import Profile from '../../utils/Profile';
+
+const updateOperationDeleteProfile = (setOp: Function, setData: Function) => {
+    return (id: number) => {
+        setOp(UpdateOperation.Delete);
+        setData({id, username: '', type: Profile.Unknown, bio: '', gender: '?'});
+    }
+}
+
+const updateOperationEditProfile = (setOp: Function, setData: Function) => {
+    return (id: number, username: string, bio: string) => {
+        setOp(UpdateOperation.Edit);
+        setData({id, username, bio, type: Profile.Unknown, gender: '?'});
+    }
+}
+
+export {updateOperationDeleteProfile, updateOperationEditProfile};
