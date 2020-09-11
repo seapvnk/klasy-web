a5d7f7d66e5c1a9ec529cc4f8957a8e639471b0a
Pedro <www.psro@gmail.com>
2020-09-11 18:21:21 -0300

    AdminCreateAccess created

---
+++ src/pages/admin/AdminCreateAccess/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import AdminPage from '../../../components/AdminPage';
+import Form from '../../../components/Form';
+
+function AdminManageClasses() {
+
+    return (
+        <AdminPage adminSection="Acesso">
+            <h1 className="text-center text-primary">Criar chave de acesso</h1>
+        </AdminPage>
+    )
+
+}
+
+export default AdminManageClasses;
