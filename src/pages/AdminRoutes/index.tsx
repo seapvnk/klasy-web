5c0b3141c319b19f2901522a965af017af9b1f3b
Pedro <www.psro@gmail.com>
2020-09-11 18:23:07 -0300

    AdminCreateAccess added to admin routes

--- src/pages/AdminRoutes/index.tsx
+++ src/pages/AdminRoutes/index.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter, Route,  } from 'react-router-dom';
 
 import AdminManagePeople from '../admin/AdminManagePeople';
 import AdminManageClasses from '../admin/AdminManageClasses';
+import AdminCreateAccess from '../admin/AdminCreateAccess';
 
 function AdminRoutes() {
     return (
         <BrowserRouter>
             <Route exact path='/admin' component={AdminManagePeople}/>
+            <Route exact path='/admin/acesso' component={AdminCreateAccess}/>
             <Route path='/admin/pessoas/:page?' component={AdminManagePeople}/>
             <Route path='/admin/turmas/:page?' component={AdminManageClasses}/>
         </BrowserRouter>
