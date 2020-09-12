bf99ab73bb89a1be867efd0688b6128168e4b1ee
Pedro <www.psro@gmail.com>
2020-09-12 12:22:22 -0300

    AdminCreateAccess updated

--- src/pages/admin/AdminCreateAccess/index.tsx
+++ src/pages/admin/AdminCreateAccess/index.tsx
@@ -7,6 +7,25 @@ function AdminManageClasses() {
     return (
         <AdminPage adminSection="Acesso">
             <h1 className="text-center text-primary">Criar chave de acesso</h1>
+            <div className="container">
+                <Form 
+                    fields={[
+                        {label: "Chave", name: "key", placeholder: "Insira a nova chave"},
+                        {label: "Tipo de acesso", name: "type", placeholder: "Tipo de acesso", type: 'select', options: {
+                            
+                            fields: [
+                                {name: 'Professor', value: 1},
+                                {name: 'Estudante', value: 2},
+                                {name: 'Administrador', value: 3},
+                            ],
+                        }},
+                        {label: "Quantidade", name: "quantity", placeholder: "Limite de uso", type: 'number', options: {
+                            min: 0,
+                        }},
+                    ]}
+                    buttonMessage="Gerar chave"
+                />
+            </div>
         </AdminPage>
     )
 
