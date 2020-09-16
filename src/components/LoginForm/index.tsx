83c64c7cb954919f6dd62e65c787136dc87580dc
Pedro <www.psro@gmail.com>
2020-09-16 14:42:24 -0300

    Axios added

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import 'spectre.css';
-
-import Navbar from '../Navbar';
-import Form from '../Form';
-import {FormFieldProps} from '../FormField';
-import styles from './styles';
 import { Link } from 'react-router-dom';
+import 'spectre.css';
+import Form from '../Form';
+import Navbar from '../Navbar';
+import { LoginFormProps } from './LoginFormProps';
+import styles from './styles';
 
-interface LoginFormProps {
-  title: string;
-  fields: Array<FormFieldProps>
-  subtitle?: string;
-  signUpPage?: boolean;
-}
 
 const LoginForm: React.FC<LoginFormProps> = ({ title, fields, subtitle, signUpPage }) => {
   const { fullscreen, pageBody, formStyle, textLarge } = styles;
