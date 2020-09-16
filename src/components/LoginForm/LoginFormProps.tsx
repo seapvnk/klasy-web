2f84e6252256eab59fa8eaf5b833cc4f3d1188b4
Pedro <www.psro@gmail.com>
2020-09-16 15:29:59 -0300

    Axios types added

--- src/components/LoginForm/LoginFormProps.tsx
+++
@@ -1,8 +0,0 @@
-import { FormFieldProps } from '../FormField';
-
-export interface LoginFormProps {
-  title: string;
-  fields: Array<FormFieldProps>;
-  subtitle?: string;
-  signUpPage?: boolean;
-}
