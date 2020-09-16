e2735284e293ec80a2c1cbefd0fdd88926b0128a
Pedro <www.psro@gmail.com>
2020-09-16 15:29:27 -0300

    Signup page function

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import LoginForm from '../../components/LoginForm';
+import api from '../../services/api';
 
 function Signup() {
+    // Handle sign up action
+    async function handleSignup( [ key, name, bio, username, password ] : Array<string>) {
+        api.post('user', { key, name, bio, username, password })
+        .then((response) => {
+            if (response.status == 400) {
+                // Account not created
+                alert('Chave inválida ou expirada');
+            } else {
+                // Account created
+                alert('Conta criada com sucesso');
+            }
+        });
+    }
     return (
         <LoginForm 
             title="Cadastro"
             subtitle="Crie um login usando a sua chave de acesso"
+            onSubmitForm={{
+                submitFunction: handleSignup,
+                submitFunctionsArgs: {},
+            }}
             fields={[
                 {label: 'Chave de acesso', name: 'key', placeholder: 'Insira sua chave de acesso'},
                 {label: 'Nome', name: 'name', placeholder: 'Seu nome'},
