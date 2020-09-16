6f3f50233f8efed56bdff480fc71e03daed70e3f
Pedro <www.psro@gmail.com>
2020-09-16 14:39:51 -0300

    login form styles created

---
+++ src/components/LoginForm/styles.ts
@@ -0,0 +1,23 @@
+const styles = {
+    fullscreen: {
+        minHeight: '100vh',
+        paddingBottom: '1em',
+    },
+    
+    pageBody: {
+        display: 'flex',
+        flexFlow: 'column',
+        alignItems: 'center',
+    },
+    
+    textLarge: {
+        fontSize: '2rem',
+        marginTop: '3rem',
+    },
+    
+    formStyle: {
+        width: '70%',
+    },
+};
+
+export default styles;
