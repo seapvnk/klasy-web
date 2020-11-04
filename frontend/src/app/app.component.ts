3a7c326f8d3f07eaaea07b02b3d368163a2fdb0a
Pedro Sérgio <www.psro@gmail.com>
2020-11-04 18:29:05 -0300

    Header component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,16 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        {{title}}!
-      </h1>
-      
-    <router-outlet></router-outlet>
-  `,
-  styles: []
+  templateUrl: 'app.component.html',
 })
 export class AppComponent {
   title = 'readism';
